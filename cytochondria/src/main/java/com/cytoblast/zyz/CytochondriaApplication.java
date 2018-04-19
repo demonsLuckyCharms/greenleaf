0bbd4072fa42c307b5f3e853351ef94af247f4ce
zhouyizhe <zyz.1224>
2018-04-19 14:47:50 +0800

    Second light

---
+++ cytochondria/src/main/java/com/cytoblast/zyz/CytochondriaApplication.java
@@ -0,0 +1,12 @@
+package com.cytoblast.zyz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CytochondriaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CytochondriaApplication.class, args);
+	}
+}
---
+++ cytochondria/src/test/java/com/cytoblast/zyz/CytochondriaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.cytoblast.zyz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CytochondriaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
